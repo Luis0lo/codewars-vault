d97bf80c5de92b6b07ed530768ac60c76f97adbf
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2023-06-22 09:29:56 +0100

    SMS Lottery Bet Validator

---
+++ 6kyu/SMSLotteryBetValidator.js
@@ -0,0 +1,31 @@
+// You were asked to write a simple validator for a company that is planning to introduce lottery betting via text messages. The same validator will be used for multiple games: e.g. 5 out of 90, 7 out of 35, etc. (N out of M)
+
+// The text messages should contain exactly N unique numbers between 1 and M (both included), separated by a comma (,) and/or space(s). Any other character makes the bet invalid.
+
+// Your task
+// You receive the game type and the user's text message as input, and you need to check if the bet is valid or not. If it's valid, return the chosen numbers as a list, sorted in increasing order. If it's invalid, return None, null, nil as appropriate to your language.
+
+// Note: Leading and trailing spaces will not be tested. Tabs, newlines and other whitespaces are not tested either. Think of a classic Nokia 3310 user for reference :-)
+
+// Examples
+// validate_bet([5, 90], "1 2 3 4 5")       -->  [1, 2, 3, 4, 5]
+// validate_bet([5, 90], "5 , 3, 1  4,2")   -->  [1, 2, 3, 4, 5]
+// validate_bet([5, 90], "1, 2; 3, 4, 5")   -->  None / null / nil
+// validate_bet([5, 90], "1, 2, 3, 4")      -->  None / null / nil
+// validate_bet([5, 90], "1, 2, 3, 4, 95")  -->  None / null / nil
+
+function validateBet(game, text){
+ 
+    const [N, M] = game;
+    const numbers = text.split(/[,\s]+/);
+  
+    if (numbers.length !== N)return null;
+  
+    const uniqueNumbers = [...new Set(numbers)];
+  
+    if (uniqueNumbers.length !== N || uniqueNumbers.some(num => !/^\d+$/.test(num) || num < 1 || num > M)) {
+      return null;
+    }
+  
+    return uniqueNumbers.map(Number).sort((a, b) => a - b);
+  }
